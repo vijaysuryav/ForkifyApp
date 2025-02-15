2877f8ec7bc904c6f1263d9675d67ef1f7b8132d
vijaysuryav <vsv10@iitbbs.ac.in>
2025-02-15 11:48:28 -0700

    MVC architecture for page load and user selects

---
+++ src/js/controller.js
@@ -0,0 +1,40 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import recipeView from './views/recipeView.js';
+
+
+
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function(){
+  try{
+
+    const id=window.location.hash.slice(1);
+
+    if(!id) return;
+    recipeView.renderSpinner();
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+
+    //2) Rendering recipe
+    recipeView.render(model.state.recipe);
+
+  
+  }catch(err){
+    recipeView.renderError();
+  }
+};
+//['hashchange','load'].forEach(ev=>window.addEventListener(ev, controlRecipes));
+const init = function(){
+  recipeView.addHandlerRender(controlRecipes);
+}
+init();
+// window.addEventListener('hashchange',controlRecipes);
+// window.addEventListener('laod',controlRecipes);
